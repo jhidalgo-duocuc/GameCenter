4b9df27352563a2d7ce92752180aca0b84ab272e
Julian Hidalgo <juli.hidalgo@duocuc.cl>
2026-05-07 21:25:01 -0400

    Push de microservicios(6-10)

---
+++ notificacion/src/test/java/cl/gamecenter/notificacion/NotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gamecenter.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
