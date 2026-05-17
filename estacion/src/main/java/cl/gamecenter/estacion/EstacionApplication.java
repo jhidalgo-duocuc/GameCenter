74a4d3674eb3fc4b8a5efb7b2875d3dc06096380
Jose Miguel <joseantinao17@gmail.com>
2026-05-17 19:46:12 -0400

    Se realiza la creacion del servicio de EUREKA, para la comunicacion del Software

--- estacion/src/main/java/cl/gamecenter/estacion/EstacionApplication.java
+++ estacion/src/main/java/cl/gamecenter/estacion/EstacionApplication.java
@@ -2,8 +2,12 @@ package cl.gamecenter.estacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class EstacionApplication {
 
 	public static void main(String[] args) {
--- membresía/src/main/java/cl/gamecenter/membresia/MembresiaApplication.java
+++ membresía/src/main/java/cl/gamecenter/membresia/MembresiaApplication.java
@@ -2,8 +2,12 @@ package cl.gamecenter.membresia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class MembresiaApplication {
 
 	public static void main(String[] args) {
