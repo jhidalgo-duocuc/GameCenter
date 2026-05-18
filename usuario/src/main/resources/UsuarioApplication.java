1c9af668c1be2b21a9d9f58755e36d03b7348c97
Jose Miguel <joseantinao17@gmail.com>
2026-05-17 23:46:54 -0400

    Se agrega el nuevo servicio de api-gateway y nuevas modificaciones en clases, se agregan dependencias para la comunicacion con el eureka clase y apigateway

--- usuario/src/main/resources/UsuarioApplication.java
+++ usuario/src/main/resources/UsuarioApplication.java
@@ -10,8 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 public class UsuarioApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UsuarioApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApplication.class, args);
+	}
 }
